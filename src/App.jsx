1593ca3d72ed3607abe995c73b2eb7aaeca4398e
Daniel Arias <danielandres.98.da@gmail.com>
2025-06-18 12:22:30 -0300

    Added dynamic correct password

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import 'antd/dist/reset.css';
 import { Layout } from 'antd';
 import SerialDisplay from './components/SerialDisplay';
+import { Footer } from 'antd/es/layout/layout';
 
 const { Header, Content } = Layout;
 
 function App() {
   return (
-    <Layout>
+    <div>
       <Header style={{ color: 'white' }}>Monitor Serie - PIC16F887</Header>
-      <Content style={{ padding: 24, minHeight: '100%' }}>
+      <Content style={{ padding: 24 }}>
         <SerialDisplay />
       </Content>
-    </Layout>
+    </div>
   );
 }
 
--- src/components/SerialDisplay.jsx
+++ src/components/SerialDisplay.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Card, Typography, message, Input } from 'antd';
+import { Card, Typography, message, Input, Button, Space } from 'antd';
 import { io } from 'socket.io-client';
 
 const { Title, Paragraph } = Typography;
@@ -9,56 +9,58 @@ const socket = io('http://localhost:8080',
   withCredentials: true,}
 );
 
-const CLAVE_CORRECTA = '4579';
-
 const SerialDisplay = () => {
-  const [mensaje, setMensaje] = useState('');
+  const [claveCorrecta, setClaveCorrecta] = useState('1234');
   const [claveIngresada, setClaveIngresada] = useState('');
+  const [disabled, setDisabled] = useState(true);
 
   useEffect(() => {
-    socket.on('serial-data', (data) => {
+    socket.on('serial-data', async(data) => {
       // Concatenamos los datos entrantes
-      const nuevaClave = claveIngresada + data;
-      setClaveIngresada(nuevaClave);
-
-      // Mostrar todo lo recibido
-      setMensaje(() => data);
-
+      const clave = await data;
+      setClaveIngresada(() => clave);
       // Si se completan los 4 caracteres, comparar
-      if (nuevaClave.length === 4) {
-        if (nuevaClave === CLAVE_CORRECTA) {
+      if (clave.length === 4) {
+        if (clave === claveCorrecta) {
           socket.emit('enviar-comando', '1');
           message.success('Clave correcta. Se envió "1" al backend');
         } else {
           socket.emit('enviar-comando', '0');
           message.error('Clave incorrecta. Se envió "0" al backend');
         }
-
-        // Reiniciar para la próxima entrada
-        setClaveIngresada('');
       }
     });
 
     return () => {
       socket.off('serial-data');
     };
-  }, [claveIngresada]);
+  }, [claveIngresada, claveCorrecta]);
+
+  const changeClaveCorrecta = (e) => {
+    setClaveCorrecta(e?.target?.value)
+  }
+
+  const changeClave = () => {
+    setDisabled(() => !disabled)
+  }
 
   return (
     <Card style={{ margin: 20 }}>
       <Title level={4}>Datos desde el PIC</Title>
-      <Paragraph code>{mensaje || 'Esperando datos...'}</Paragraph>
-
       <Paragraph>
         Clave actual recibida: <strong>{claveIngresada}</strong>
       </Paragraph>
-
-      <Input
-        style={{ maxWidth: 200 }}
-        value={CLAVE_CORRECTA}
-        disabled
-        addonBefore="Clave esperada"
-      />
+      <Space size={15}>
+        <Input
+          style={{ maxWidth: 200 }}
+          value={claveCorrecta}
+          addonBefore="Clave esperada"
+          onChange={changeClaveCorrecta}
+          disabled={disabled}
+          maxLength={4}
+        />
+        <Button type="primary" onClick={changeClave}>{disabled? 'Cambiar clave' : 'Aceptar'}</Button>
+      </Space>
     </Card>
   );
 };
